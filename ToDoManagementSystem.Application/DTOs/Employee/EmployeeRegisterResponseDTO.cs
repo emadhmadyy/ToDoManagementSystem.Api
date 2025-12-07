98e9c992472b94945fbf0a5cd86fed611b70c5ed
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 17:21:25 +0200

    Add Employee DTOs in Application layer

---
+++ ToDoManagementSystem.Application/DTOs/Employee/EmployeeRegisterResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoManagementSystem.Application.DTOs.Employee
+{
+    public class EmployeeRegisterResponseDTO
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+    }
+}
