e60a4ec5213b9641921fb725c127df6143d7c235
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 16:57:21 +0200

    Add TodoStatus enum in Core layer

---
+++ ToDoManagementSystem.Core/Enums/TodoStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoManagementSystem.Core.Enums
+{
+    public enum TodoStatus
+    {
+        Pending = 0,
+        InProgress = 1,
+        Done = 2
+    }
+}
