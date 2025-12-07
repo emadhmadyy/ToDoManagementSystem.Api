d207a4daef2323509e78fca41214a942eae55085
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 20:44:22 +0200

    Add JwtTokenGenerator interface to application layer and implement it in Infrastructure layer

--- ToDoManagementSystem.Infrastructure/DependencyInjection.cs
+++ ToDoManagementSystem.Infrastructure/DependencyInjection.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using ToDoManagementSystem.Application.Interfaces.Security;
+using ToDoManagementSystem.Infrastructure.Configuration;
 using ToDoManagementSystem.Infrastructure.Security;
 
 namespace ToDoManagementSystem.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructureDI(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructureDI(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddSingleton<IPasswordHasher, BCryptPasswordHasher>();
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+            services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
             return services;
         }
     }
---
+++ ToDoManagementSystem.Infrastructure/Security/JwtTokenGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using ToDoManagementSystem.Application.Interfaces.Security;
+using ToDoManagementSystem.Infrastructure.Configuration;
+
+namespace ToDoManagementSystem.Infrastructure.Security
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JwtSettings _settings;
+
+        public JwtTokenGenerator(IOptions<JwtSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+        public string GenerateJwtToken(string employeeId, string employeeEmail, string employeeName)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, employeeId),
+                new Claim(JwtRegisteredClaimNames.Email, employeeEmail),
+                new Claim(JwtRegisteredClaimNames.UniqueName, employeeName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_settings.SecretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _settings.Issuer,
+                audience: _settings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
