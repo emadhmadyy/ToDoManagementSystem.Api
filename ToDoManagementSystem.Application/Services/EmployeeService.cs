890b13bf1517d776f0f917b5d8f6508438488a35
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 18:28:29 +0200

    Add EmployeeService class and TodoService class in Application layer

---
+++ ToDoManagementSystem.Application/Services/EmployeeService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoManagementSystem.Application.DTOs.Employee;
+using ToDoManagementSystem.Application.Interfaces.Services;
+
+namespace ToDoManagementSystem.Application.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public Task<EmployeeLoginRequestDTO> LoginEmployeeAsync(EmployeeLoginRequestDTO request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EmployeeRegisterResponseDTO> RegisterEmployeeAsync(EmployeeRegisterRequestDTO request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ToDoManagementSystem.Application/Services/TodoService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoManagementSystem.Application.DTOs.Todo;
+using ToDoManagementSystem.Application.Interfaces.Services;
+
+namespace ToDoManagementSystem.Application.Services
+{
+    public class TodoService : ITodoService
+    {
+        public Task<TodoResponseDTO> CreateTodoAsync(TodoCreateRequestDTO request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTodoAsync(string EmployeeId, string TodoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TodoResponseDTO>> GetAllTodosAsync(string EmployeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoResponseDTO> UpdateTodoAsync(TodoUpdateRequestDTO request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
