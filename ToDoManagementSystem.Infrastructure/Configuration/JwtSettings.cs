010843773a230897a55b0fbbc500e3a356ddc3f2
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 20:42:44 +0200

    Add Jwt settings in app settings and secret-key to user secrets

---
+++ ToDoManagementSystem.Infrastructure/Configuration/JwtSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoManagementSystem.Infrastructure.Configuration
+{
+    public class JwtSettings
+    {
+        public string Issuer { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+        public string SecretKey { get; set; } = null!;
+    }
+}
