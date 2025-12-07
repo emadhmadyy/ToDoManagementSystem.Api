4bf2df2f20e21977550a96a9008bee977ce5811b
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 18:14:18 +0200

    Add todo service interface

---
+++ ToDoManagementSystem.Application/Interfaces/Services/ITodoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoManagementSystem.Application.DTOs.Todo;
+
+namespace ToDoManagementSystem.Application.Interfaces.Services
+{
+    public interface ITodoService
+    {
+        Task<TodoResponseDTO> CreateTodoAsync(TodoCreateRequestDTO request);
+        Task<TodoResponseDTO> UpdateTodoAsync(TodoUpdateRequestDTO request);
+        Task<List<TodoResponseDTO>> GetAllTodosAsync(string EmployeeId);
+        Task DeleteTodoAsync(string EmployeeId, string TodoId);
+    }
+}
