5abe0d91223ba77928c7af85b6999f5bee64c370
Emad Hmady <emadhmady0@gmail.com>
2025-12-08 01:53:24 +0200

    Add EmployeesController in Api layer

---
+++ ToDoManagementSystem.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoManagementSystem.Application.DTOs.Employee;
+using ToDoManagementSystem.Application.Interfaces.Services;
+
+namespace ToDoManagementSystem.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController: ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(EmployeeRegisterRequestDTO request)
+        {
+            var response = await _employeeService.RegisterEmployeeAsync(request);
+            return CreatedAtAction(nameof(Register), new { id = response.Id }, response);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(EmployeeLoginRequestDTO request)
+        {
+            var response = await _employeeService.LoginEmployeeAsync(request);
+            return Ok(response);
+        }
+    }
+}
