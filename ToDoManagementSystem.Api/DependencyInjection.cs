638707944d76c46a33d9437fdba416626de9bbf0
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 16:40:41 +0200

    Add DI configuration files for Application, Infrastructure, and API layers

---
+++ ToDoManagementSystem.Api/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using ToDoManagementSystem.Application;
+using ToDoManagementSystem.Infrastructure;
+
+namespace ToDoManagementSystem.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI().AddInfrastructureDI();
+            return services;
+        }
+    }
+}
--- ToDoManagementSystem.Api/Program.cs
+++ ToDoManagementSystem.Api/Program.cs
@@ -1,9 +1,12 @@
+using ToDoManagementSystem.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAppDI();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
