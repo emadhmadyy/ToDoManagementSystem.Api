e44423c8d1ae3b7e0d2251d3affe156d5479ff41
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 20:55:23 +0200

    Add EmployeeRepository Implementation in Infrastructure layer

---
+++ ToDoManagementSystem.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoManagementSystem.Application.Interfaces.Repositories;
+using ToDoManagementSystem.Core.Entities;
+
+namespace ToDoManagementSystem.Infrastructure.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public Task CreateEmployeeAsync(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetByEmailAsync(string employeeEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetByIdAsync(string employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEmployeeAsync(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
