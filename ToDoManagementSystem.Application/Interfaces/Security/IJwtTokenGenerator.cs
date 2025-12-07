9db879121aa4192d88b2b5ae550211ef0c8b535a
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 20:10:35 +0200

    Add JwtTokenGenerator interface

---
+++ ToDoManagementSystem.Application/Interfaces/Security/IJwtTokenGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoManagementSystem.Application.Interfaces.Security
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateJwtToken(string employeeId, string employeeEmail, string employeeName);
+    }
+}
