a577815d1a38cf9283afd00d083d4953182e5038
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 18:04:54 +0200

    Add employee service interface

---
+++ ToDoManagementSystem.Application/Interfaces/Services/IEmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoManagementSystem.Application.DTOs.Employee;
+
+namespace ToDoManagementSystem.Application.Interfaces.Services
+{
+    public interface IEmployeeService
+    {
+        Task<EmployeeRegisterResponseDTO> RegisterEmployee(EmployeeRegisterRequestDTO request);
+        Task<EmployeeLoginRequestDTO> LoginEmployee(EmployeeLoginRequestDTO request);
+    }
+}
