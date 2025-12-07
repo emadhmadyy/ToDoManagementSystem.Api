8fab7f22806d2ddd5c77919db7a4c957b15e3bee
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 17:54:19 +0200

    Add Todo DTOs in Application layer

---
+++ ToDoManagementSystem.Application/DTOs/Todo/TodoCreateRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoManagementSystem.Application.DTOs.Todo
+{
+    public class TodoCreateRequestDTO
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime DueDate { get; set; }
+        public int Priority { get; set; }
+    }
+}
