1db624c0d290efad4aebcf556c846b9e63211981
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 16:55:41 +0200

    Add Employee entity in Core layer

---
+++ ToDoManagementSystem.Core/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoManagementSystem.Core.Entities
+{
+    public class Employee
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public List<Todo> Todos { get; set; } = [];
+    }
+}
