0e563b361d5c054f881b920097354d2e71335b71
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 18:40:15 +0200

    Implement PasswordHasher interface in infrastructure layer

---
+++ ToDoManagementSystem.Infrastructure/Security/BCryptPasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoManagementSystem.Application.Interfaces.Security;
+
+namespace ToDoManagementSystem.Infrastructure.Security
+{
+    internal class BCryptPasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
