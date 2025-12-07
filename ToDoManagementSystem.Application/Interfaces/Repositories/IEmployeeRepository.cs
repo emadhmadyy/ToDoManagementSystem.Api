a4be232f3294fdd5846580f83809568549947023
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 18:22:29 +0200

    Add employee repository interface in application layer

---
+++ ToDoManagementSystem.Application/Interfaces/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoManagementSystem.Core.Entities;
+
+namespace ToDoManagementSystem.Application.Interfaces.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<Employee> GetByIdAsync(string EmployeeId);
+        Task CreateEmployee(Employee employee);
+        Task UpdateEmployee(Employee employee);
+    }
+}
