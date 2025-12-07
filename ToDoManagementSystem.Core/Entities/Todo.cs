e211d319ee69dfb7a878dcb612dc783c95f58bc5
Emad Hmady <emadhmady0@gmail.com>
2025-12-07 16:56:25 +0200

    Add Todo entity in Core layer

---
+++ ToDoManagementSystem.Core/Entities/Todo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoManagementSystem.Core.Enums;
+
+namespace ToDoManagementSystem.Core.Entities
+{
+    public class Todo
+    {
+        public string Id { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime DueDate { get; set; }
+        public int Priority { get; set; }
+        public TodoStatus Status { get; set; } = TodoStatus.Pending;
+
+    }
+}
