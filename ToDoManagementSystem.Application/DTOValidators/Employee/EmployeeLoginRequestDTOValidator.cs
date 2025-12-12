b0e09afa81721be205fc5dfb121bb5fbdb671070
Emad Hmady <emadhmady0@gmail.com>
2025-12-12 02:28:58 +0200

    Remove Match rule from password in EmployeeLoginRequestDTOValidator

--- ToDoManagementSystem.Application/DTOValidators/Employee/EmployeeLoginRequestDTOValidator.cs
+++ ToDoManagementSystem.Application/DTOValidators/Employee/EmployeeLoginRequestDTOValidator.cs
@@ -8,11 +8,7 @@ namespace ToDoManagementSystem.Application.DTOValidators.Employee
         public EmployeeLoginRequestDTOValidator()
         {
             RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required").EmailAddress().WithMessage("Invalid email format");
-            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required").MinimumLength(8).WithMessage("Password must be at least 8 characters")
-                .Matches(@"[A-Z]").WithMessage("Password must include at least on uppercase letter")
-                .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
-                .Matches(@"\d").WithMessage("Password must contain at least one number")
-                .Matches(@"[@$!%*?&]").WithMessage("Password must contain at least one special character");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
         }
     }
 }
--- ToDoManagementSystem.Application/DTOs/Employee/EmployeeLoginResponseDTO.cs
+++ ToDoManagementSystem.Application/DTOs/Employee/EmployeeLoginResponseDTO.cs
@@ -8,9 +8,7 @@ namespace ToDoManagementSystem.Application.DTOs.Employee
 {
     public class EmployeeLoginResponseDTO
     {
-        public string Id { get; set; } = null!;
         public string Name { get; set; } = null!;
-        public string Email { get; set; } = null!;
         public string Token { get; set; } = null!;
     }
 }
